fb29361ed040fa1b329cfce0d89fb9bdc0f94e10
Chakhrit_017 <chakhritz@gmail.com>
2016-12-18 17:17:28 +0700

    no message

---
+++ MapMVCPro/MapMVCPro/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using MapMVCPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapMVCPro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyDatabaseEntities db = new MyDatabaseEntities();
+
+        public ActionResult Index(string searchString)
+        {
+            var locations = from l in db.Locations
+                            select l;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                locations = locations.Where(l => l.Title.Contains(searchString));
+            }
+            return View(locations);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Quote form our CEO ";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contace We";
+
+            return View();
+        }
+        public JsonResult GetAllLocation()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Locations.OrderBy(a => a.Title).ToList();
+                return new JsonResult { Data = v, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+        public JsonResult GetMarkerInfo(int locationID)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                Location l = null;
+                l = dc.Locations.Where(a => a.LocationID.Equals(locationID)).FirstOrDefault();
+                return new JsonResult { Data = l, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
---
+++ MapMVCPro/MapMVCPro/Controllers/LocationsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MapMVCPro.Models;
+
+namespace MapMVCPro.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private MyDatabaseEntities db = new MyDatabaseEntities();
+
+        // GET: Locations
+        public ActionResult Index(string searchString)
+        {
+            var locations = from l in db.Locations
+                            select l;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                locations = locations.Where(s => s.Title.Contains(searchString));
+            }
+            return View(locations);
+        }
+
+        // GET: Locations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return HttpNotFound();
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LocationID,Title,Lat,Long,Address,Phone,ImagePath,Detail,Price,Bed,Wardrobe,Conditioner,Desk,Toilet,Refrigrator,Wifi")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Locations.Add(location);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(location);
+        }
+
+        // GET: Locations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return HttpNotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Locations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LocationID,Title,Lat,Long,Address,Phone,ImagePath,Detail,Price,Bed,Wardrobe,Conditioner,Desk,Toilet,Refrigrator,Wifi")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(location).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return HttpNotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Location location = db.Locations.Find(id);
+            db.Locations.Remove(location);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
